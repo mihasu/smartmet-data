5fd733ce570a10ea7e5006428fd2535345486dee
Mikael <117453127+mihasu@users.noreply.github.com>
2025-01-30 14:31:40 +0200

    Added station numbers

--- local_obs/bin/do_local_obs.sh
+++ local_obs/bin/do_local_obs.sh
@@ -12,18 +12,17 @@ WORKING_FILE=${WORKING_DIR}/local_obs_${TIMESTAMP}.csv;
 QD_FILE=${SMARTMET_DIR}/${TIMESTAMP}_tajikistan_local_obs.sqd
 
 echo "getting station data for db at $PWD DATE: $TIMESTAMP";
-curl -s $STATION_URL | jq -r '.[] | [.siteID, .siteID, .longitude, .latitude, .siteName] | @csv' | sed 's/"//g' > $STATION_FILE
+curl -s $STATION_URL | jq -r '.[] | [.longitude, .latitude, .siteID] | @csv' | sed 's/"//g' | awk '{print NR "," NR "," $0}' > $STATION_FILE
 
 echo "Downloading site data for each station..."
-tail -n +2 $STATION_FILE | while IFS=, read -r siteID _; do
+tail -n +2 $STATION_FILE | while IFS=, read -r stationID stationN longitude latitude siteID; do
     echo "Processing siteID: $siteID"
-    curl -s "${DATA_URL}${siteID}" | jq -r '.[] |   
-        [   .siteID,
+    curl -s "${DATA_URL}${siteID}" | jq -r --arg stationID "$stationID" '.[] |   
+        [   $stationID,
             (.datetime | strptime("%a, %d %b %Y %H:%M:%S %Z") | strftime("%Y%m%dT%H%M%S")),
             (.data[] | select(.machineName == "air_temperature") | .value // ""),
             (.data[] | select(.machineName == "relative_humidity") | .value // ""),
             (.data[] | select(.machineName == "air_pressure") | .value // ""),
-            (.data[] | select(.machineName == "dew_point") | .value // ""),
             (.data[] | select(.machineName == "precipitation") | .value // ""),
             (.data[] | select(.machineName == "wind_direction") | .value // ""),
             (.data[] | select(.machineName == "wind_speed") | .value // "")
@@ -33,6 +32,6 @@ done
 echo "Replacing precipitation -999 with empty string";
 sed -i 's/-999//g' ${WORKING_FILE};echo "Executing csv2qd";
 echo $QD_FILE
-csv2qd -v -S ${STATION_FILE} --order=idtime -p Temperature,Humidity,Pressure,DewPoint,PrecipitationAmount,WindDirection,WindSpeedMS -i ${WORKING_FILE} -o ${QD_FILE}
+csv2qd -v -S ${STATION_FILE} --order=idtime -p Temperature,Humidity,Pressure,PrecipitationAmount,WindDirection,WindSpeedMS -i ${WORKING_FILE} -o ${QD_FILE}
 
 echo "Resulting file: ${QD_FILE}";
